659fdeabd7ba5c8c1745f160f6ef51e94485186d
Charles MILLEQUANT <cmdarkmole@gmail.com>
2017-01-20 14:53:03 +0100

    BugFix
    Some bug fixes

--- page2/page2.component.js
+++ page2/page2.component.js
@@ -2,7 +2,7 @@ function Page2Controller(){
     console.log('Page 2 controller');
 }
 
-Page1Controller.$inject = [];
+Page2Controller.$inject = [];
 
 angular.module('demoApp.page2Component', []).component('page2Component', {
     templateUrl: 'page2/page2.component.html',
