64f3b43beaf8cc3c5c3bfb221bf91141fb9dc7a3
Charles MILLEQUANT <cmdarkmole@gmail.com>
2017-01-20 14:54:25 +0100

    Revert "BugFix"
    This reverts commit 659fdeabd7ba5c8c1745f160f6ef51e94485186d.

--- page2/page2.component.js
+++ page2/page2.component.js
@@ -2,7 +2,7 @@ function Page2Controller(){
     console.log('Page 2 controller');
 }
 
-Page2Controller.$inject = [];
+Page1Controller.$inject = [];
 
 angular.module('demoApp.page2Component', []).component('page2Component', {
     templateUrl: 'page2/page2.component.html',
